4b8971ccd6473ce300958e5a837fd693e99a8203
Ion Alexandru <ion.1alexandruleonard@yahoo.ro>
2025-01-22 22:47:41 +0200

    requests controller done?

--- back/controllers/request.js
+++ back/controllers/request.js
@@ -21,10 +21,47 @@ const controller = {
         
     },
     acceptRequest: async(req, res) => {
-
+        try {
+            let evaluatedRequest = await RequestModel.findByPk(req.params.id)
+            if(evaluatedRequest){
+                let updatedRequest = await RequestModel.update({
+                    wasApproved: true,
+                    appTitle: appTitle,
+                    appDescription: appDescription,
+                    denialJustification: null
+                }, {where: {id:req.params.id}})
+            
+            let sessionUpdated = await SessionModel.update({
+                availableSlots: availableSlots -= 1
+            }, {where: {id: updatedRequest.sessionId}})
+            
+            } else {
+                res.status(404).json("No request found")
+            }
+        } catch (error) {
+            console.warn("error: accept request")
+            res.status(500).json("error: accept request")
+        }
     },
+    //params: id, body: denialJustification
     denyRequest: async(req, res) => {
-
+        try {
+            let evaluatedRequest = await RequestModel.findByPk(req.params.id)
+            if(evaluatedRequest){
+                let updatedRequest = await RequestModel.update({
+                    wasApproved: false,
+                    appTitle: appTitle,
+                    appDescription: appDescription,
+                    denialJustification: req.body.denialJustification
+                }, {where: {id:req.params.id}})
+                res.status(200).json("Request denied successfully")
+            } else {
+                res.status(404).json("No request found")
+            }
+        } catch (error) {
+            console.warn("error: deny request")
+            res.status(500).json("error: deny request")
+        }
     }
 }
 
